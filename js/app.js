f655ec70a278ec0eb5c199c38b331e8a42d45d6e
XurshidovichBekzod <xurshidovichbekzod@gmail.com>
2025-05-16 21:07:02 +0500

    codeWors

---
+++ js/app.js
@@ -0,0 +1,84 @@
+{
+    //Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+    //1
+    function greet(name) {
+        return "Hello, " + name + " how are you doing today?";
+    }
+}
+
+{
+    //It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string
+    //2
+    {
+        function removeChar(str) {
+            return str.slice(1, -1);
+        }
+    }
+
+}
+
+{
+    //3
+    {
+        function findSmallestInt(arr) {
+            return Math.min(...arr);
+        }
+    }
+}
+{
+    //Given an array of integers your solution should find the smallest integer.
+    //4
+    {
+        function findSmallestInt(arr) {
+            let smallest = arr[0];
+            for (let i = 1; i < arr.length; i++) {
+                if (arr[i] < smallest) {
+                    smallest = arr[i];
+                }
+            }
+            return smallest;
+        }
+
+    }
+}
+
+{
+    //Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+    //5 
+    {
+        function stringToNumber(str) {
+            return Number(str);
+        }
+    }
+}
+
+{
+    //6
+    {
+        function doubleArray(arr) {
+            return arr.map(num => num * 2);
+        }
+    }
+}
+
+{
+    //Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+    //7
+    function countPositivesSumNegatives(input) {
+        if (!input || input.length === 0) return [];
+
+        let countPositives = 0;
+        let sumNegatives = 0;
+
+        for (let num of input) {
+            if (num > 0) {
+                countPositives++;
+            } else if (num < 0) {
+                sumNegatives += num;
+            }
+        }
+
+        return [countPositives, sumNegatives];
+    }
+}
+
